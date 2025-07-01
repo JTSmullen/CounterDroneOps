b65083798ef4094bdc2c10e6a8a980f868a89092
Joshua Smullen <158128671+JTSmullen@users.noreply.github.com>
2025-07-01 16:13:07 -0400

    [Add] -> Core Functionality Overhaul
    Adding core functional aspects of the C4001 Sensor. Including range and other telemetry data. 
    
    Added Robust Documentation of all functions and objects.

--- ESP32_base_MQTT_protocol/Sensor_node_1/Sensor_node_1.ino
+++ ESP32_base_MQTT_protocol/Sensor_node_1/Sensor_node_1.ino
@@ -2,12 +2,14 @@
 #include <DFRobot_C4001.h> // Connect to the C4001 sensor
 #include <WiFi.h> // Connect to the wifi for mqtt communication
 #include <PubSubClient.h> // mqtt communication library
+// --- End of Import Statements
 
 // --- PIN DEFINITIONS ---
 #define RX_PIN 19 // C4001 RX (C/R) PIN
 #define TX_PIN 18 // C4001 TX (D/T) PIN
+// --- End of Pin Definitions ---
 
-// --- WIFI CONFIG ---
+// --- WIFI and MQTT Config ---
 const char* WIFI_SSID = ""; // Your WiFi network
 const char* WIFI_PASSWORD = ""; // Your WiFi password
 
@@ -15,22 +17,38 @@ const char* WIFI_PASSWORD = ""; // Your WiFi password
 const char* MQTT_SERVER = ""; // Your Raspberry Pi's IP address on your home network
 // To get the this run 'hostname -I' on your Pi's terminal
 const int MQTT_PORT = 1883; // General port for MQTT communication
+// ---End of WiFi and MQTT Config ---
 
 // --- SENSOR NODE CONFIG ---
 const int NODE_ID = 1; // Which sensor node of the array
 const char* MQTT_TOPIC = "sensors/radar/status"; // Topic to publish to
+// --- End of Sensor Node Config ---
 
 // --- GLOBAL OBJECTS ---
 DFRobot_C4001_UART radar(&Serial1, 9600, RX_PIN, TX_PIN);
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
+// --- End of Global Objects ---
 
-// --- Connect to WiFi ---
+// --- Functions ---
+/*
+    * @name setup_wifi
+
+    * @brief Function to connect to WiFi.
+
+    * @details This function will connect to the specified WiFi network using the provided SSID
+
+    * @note This function will continuously attempt until it connects.
+    * @note This function will block until the connection is established.
+    * @note This function should be called in the setup() function.
+
+    * @return void
+*/
 void setup_wifi()
 {
   delay(10);
   Serial.println();
-  Serial.print("Connection to ");
+  Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -47,8 +65,21 @@ void setup_wifi()
   Serial.println(WiFi.localIP());
 }
 
+/*
+    * @name reconnect_mqtt
+
+    * @brief Function to connect to the MQTT broker.
+
+    * @details This function will connect to the MQTT broker using the provided server and port.
+
+    * @note This function will continuously attempt until it connects.
+    * @note This function will block until the connection is established.
+    * @note This function should be called in the loop() function.
+
+    * @return void
+*/
 void reconnect_mqtt() 
-  {
+{
   while (!mqttClient.connected()) 
   {
     Serial.print("Attempting MQTT connection...");
@@ -68,38 +99,58 @@ void reconnect_mqtt()
   }
 }
 
-void publishMotionData()
-{
-  String payload = "{\"nodeId\":" + String(NODE_ID) + ",\"status\":\"motion_detected\"}";
+/*
+    * @name publishMotionData
 
-  Serial.print("Publishing message to topic: ");
-  Serial.println(MQTT_TOPIC);
-  Serial.println(payload);
+    * @brief Function to publish motion data to the MQTT broker.
 
-  mqttClient.publish(MQTT_TOPIC, payload.c_str());
-}
+    * @details This function will create a JSON payload with the node ID and status, then publish it to the specified MQTT topic.
 
+    * @note This function should be called when motion is detected.
+    * @note This functions is used to send a no motion detected Json Payload
+
+    * @return void
+*/
 void publishStatus(const char* status)
 {
   String payload = "{\"nodeId\":" + String(NODE_ID) + ",\"status\":\"" + status + "\"}";
+  
+  Serial.print("Publishing message to topic: ");
+  Serial.println(MQTT_TOPIC);
+  Serial.println(payload);
+  
   mqttClient.publish(MQTT_TOPIC, payload.c_str());
 }
 
+/*
+    * @name setup
+
+    * @brief Function to initialize the sensor node.
+
+    * @details This function will set up the serial communication, connect to WiFi, and initialize the MQTT client.
+
+    * @note This function should be called once at the start of the program.
+
+    * @return void
+*/
 void setup()
 {
+  // Baud rate for serial communication
   Serial.begin(115200);
+
+  // Ensure the Serial port is ready
   while (!Serial)
   {
     delay(10);
   }
   
-  // connect to WiFi
+  // Initializes WiFi connection
   setup_wifi();
 
-  // Config MQTT
+  // Initializes MQTT Client
   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
 
-  // init radar
+  // Initializes the radar sensor and ensures its connected
   while (!radar.begin())
   {
     Serial.println("C4001 not detected!");
@@ -107,8 +158,14 @@ void setup()
   }
   Serial.println("C4001 connected!");
 
-  radar.setSensorMode(eExitMode);
+  radar.setSensorMode(eSpeedMode);
 
+  radar.setDetectThres(60, 1200, 50); // range an object that is detected
+  radar.setDetectionRange(60, 1200, 1200); // Range the c4001 scans in for motion (in cms)
+  radar.setTrigSensitivity(5); // Trigger Sensitivity
+  radar.setKeepSensitivity(5); // Keep-locked-on sensitivity (determines when a moving object is no longer detected)
+
+  // Print radar sensor status
   sSensorStatus_t status = radar.getStatus();
   Serial.print("Work status: ");
   Serial.println(status.workStatus);
@@ -118,23 +175,56 @@ void setup()
   Serial.print(status.initStatus);
 }
 
+/*
+    * @name loop
+
+    * @brief Main loop function to handle sensor data and MQTT communication.
+
+    * @details This function will continuously check for motion detection, publish status updates, and maintain the MQTT connection.
+
+    * @note This function should be called repeatedly in the main program loop.
+
+    * @return void
+*/
 void loop()
 {
+  // Ensure the mqtt client is connected
+  // if not try to reconnect
   if (!mqttClient.connected())
   {
     reconnect_mqtt();
   }
 
+  // Loop the MQTT Client
   mqttClient.loop();
 
+  // Check if the radar sensor detects motion
   if (radar.motionDetection())
   {
-    publishStatus("motion_detected");
+    // Motion detected, get data from the sensor
+    int targetNum = radar.getTargetNumber();
+    float targetRange = radar.getTargetRange();
+    float targetSpeed = radar.getTargetSpeed();
+
+    // Create a detailed JSON Payload to send the receiver
+    String payload = "{\"nodeId\":" + String(NODE_ID) +
+                     ",\"status\":\"motion_detected\"" +
+                     ",\"targetNumber\":" + String(targetNum) +
+                     ",\"range_cm\":" + String(targetRange) +
+                     ",\"speed_m_s\":" + String(targetSpeed) + "}";
+
+    // Print the publishing message
+    Serial.print("Publishing message to topic: ");
+    Serial.println(MQTT_TOPIC);
+    Serial.println(payload);
+    
+    mqttClient.publish(MQTT_TOPIC, payload.c_str());
   }
   else
   {
+    // No motion is detected
     publishStatus("no_motion");
   }
 
-  delay(1000);
+  delay(1000); // Check for motion every 1 second to not overload the MQTT Broker
 }
